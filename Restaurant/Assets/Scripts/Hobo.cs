3ce110097c646e82207dc0d97849cbc0059ebbf1
Evan <elahurd3@gatech.edu>
2014-07-03 03:31:42 -0400

    added more sounds/objects
    added hobo sounds, car w/sound, bird ambient sound, set up restaurant
    layout

--- Restaurant/Assets/Scripts/Hobo.cs
+++ Restaurant/Assets/Scripts/Hobo.cs
@@ -9,6 +9,20 @@ public class Hobo : MonoBehaviour {
 	private bool toEnd;
 	private bool isMoving;
 
+	private AudioClip[] beg;
+	public AudioClip mumble;
+	public AudioClip fiftycent;
+	public AudioClip hamburger;
+	public AudioClip carBroke;
+	public AudioClip cocaCola;
+	private float nextClip;
+
+	private float begTime;
+	private bool addToBegTime;
+
+	private bool startMumbling;
+
+
 	// Use this for initialization
 	void Start () { 
 		start = new Vector3 (-19f, .879286f, 8f);
@@ -16,10 +30,34 @@ public class Hobo : MonoBehaviour {
 		toStart = false;
 		toEnd = true;
 		isMoving = true;
+
+		begTime = 0f; //keep track of time that hobo begs so that mumbling and begging don't overlap
+
+		startMumbling = false;
+
+		beg = new AudioClip[4];
+		beg [0] = fiftycent;
+		beg [1] = hamburger;
+		beg [2] = carBroke;
+		beg [3] = cocaCola;
+		nextClip = 0f;
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
+		begTime += Time.deltaTime;
+
+		//start mumbling again after done begging and Player has left trigger zone
+		if (!audio.isPlaying && begTime > 1.35f && startMumbling) {
+			audio.clip = mumble;
+			audio.loop = true;
+			audio.Play ();
+		}
+
 		if (isMoving) {
 			if ((Mathf.Abs(transform.position.x - end.x) > 0.1) && toEnd) {
 				transform.Translate(new Vector3(-0.05f,0f,0f));
@@ -38,12 +76,25 @@ public class Hobo : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.tag == "Player")
+		if (other.gameObject.tag == "Player") {
 			isMoving = false;
+			startMumbling = false;
+			begTime = 0f; //reset beg time counter
+
+			//stop mumbling and beg (don't stop if he's still begging from before)
+			if (audio.isPlaying && audio.clip == mumble) {
+				audio.Stop();
+				audio.loop = false;
+				audio.clip = beg[(int)Mathf.Round(Random.value*3.0f)];
+				audio.Play();
+			}
+		}
 	}
 	void OnTriggerExit(Collider other) {
-		if (other.gameObject.tag == "Player")
-			isMoving = true;
+		if (other.gameObject.tag == "Player") {
+			isMoving = true; //start moving again
+			startMumbling = true; //start mumbling after Player has left trigger zone
+		}
 	}
 	
 
